92e9dc9bec9d9d31a4f4fe5655dd982973b79c96
Chris Tsui <chris.tsui112@outlook.com>
2020-08-20 16:36:35 -0700

    prevent multiply .

--- code.js
+++ code.js
@@ -1,49 +1,92 @@
 let numbers = {operand:"",
                operator:"",
-               result:0
 };
 function controller(e){
-    let setOperators = new Set(["+","-","*","/"]);
+    let operators = new Set(["+","-","*","/"]);
+    let allowedChar = ["0","1","2","3","4","5","6","7","8","9","."];
+    let noPeriodsAllowed= ["0","1","2","3","4","5","6","7","8","9"];
     let display = document.querySelector('textarea');
-    if (e.target.value === "=" && numbers.operand !== ""){
+    let input = e.target.value;
+    if (input === "=" && numbers.operand !== ""){
         switch(numbers.operator){
             case "+":
                 display.textContent = Number(numbers.operand) + Number(display.textContent);
                 numbers.operand ="";
+                numbers.operator="";
                 break;
             case "-":
                 display.textContent = Number(numbers.operand) - Number(display.textContent);
                 numbers.operand="";
+                numbers.operator="";
                 break;
             case "*":
+                console.log();
                 display.textContent = Number(numbers.operand) * Number(display.textContent);
                 numbers.operand="";
+                numbers.operator="";
                 break;
             case "/":
                 display.textContent = Number(numbers.operand) / Number(display.textContent);
                 numbers.operand="";
+                numbers.operator="";
                 break;
         }
-    } else if (setOperators.has(e.target.value)){
+    } else if (operators.has(input)){
         if (numbers.operand === ""){
                 numbers.operand= display.textContent;
-                numbers.operator = e.target.value;
+                numbers.operator = input;
                 display.textContent="";
             }else{
-                numbers.operand= Number(numbers.operand)+Number(display.textContent);
-                numbers.operator=e.target.value;
-                display.textContent="";
+                switch(numbers.operator){
+                    case "+":
+                        numbers.operand= Number(numbers.operand)+Number(display.textContent);
+                        numbers.operator=input;
+                        display.textContent="";
+                        break;
+                    case "-":
+                        numbers.operand= Number(numbers.operand)-Number(display.textContent);
+                        numbers.operator=input;
+                        display.textContent="";
+                        break;
+                    case "*":
+                        numbers.operand= Number(numbers.operand)*Number(display.textContent);
+                        numbers.operator=input;
+                        display.textContent="";
+                        break;
+                    case "/":
+                        numbers.operand= Number(numbers.operand)/Number(display.textContent);
+                        numbers.operator=input;
+                        display.textContent="";
+                        break;
+                }
+                // numbers.operand= Number(numbers.operand)+Number(display.textContent);
+                // numbers.operator=input;
+                // display.textContent="";
             }
-    } else{
-        display.textContent += e.target.value;
+    } else {
+        if ((display.textContent.includes(".") && input===".")||(input==="=")){
+
+        }  else{
+            if (allowedChar.includes(input)){display.textContent += input;}
+        }
     }
 }
+function countDecimals(){
 
+}
+function clear(){
+    let textArea = document.querySelector('textarea');
+    textArea.textContent="";
+    numbers.operand="";
+    numbers.operator="";
+}
 function init(){
     let div = document.querySelector('div');
     div.addEventListener("click", event=>{
         controller(event);
     })
+    let clearButton = document.getElementById('clear');
+    clearButton.onclick=clear;
 }
 window.onload= init;
 
