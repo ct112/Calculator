e6b1053c9f3cb468191bcd9ba86e5fc909f8da0e
Chris Tsui <chris.tsui112@outlook.com>
2020-09-03 14:55:28 -0700

    added 0 to display

--- code.js
+++ code.js
@@ -8,9 +8,11 @@ let operator = "";
 let lastOperatorIsEquals= false;
 
 function appendNumber(key){
+    if (firstOperand.textContent==="0"){firstOperand.textContent=""}
     if ((firstOperand.textContent.includes(".") && key ===".")||lastOperatorIsEquals === true){return}
     firstOperand.textContent += key;
 }
+
 function calculate(x,y){
     const calcChoice = {
         "+": (x, y) => {return x + y},
@@ -20,35 +22,40 @@ function calculate(x,y){
     }
     return calcChoice[operator](x,y);
 }
+
 function isExpressionReady(e){
     if (firstOperand.textContent === ""){return};
     if (secondOperand.textContent === "") {
             secondOperand.textContent = firstOperand.textContent;
             operator = e.target.textContent;
-            firstOperand.textContent = "";
+            firstOperand.textContent = "0";
         }else{
             secondOperand.textContent= calculate(Number(secondOperand.textContent),Number(firstOperand.textContent));
             operator=e.target.textContent;
-            firstOperand.textContent ="";
+            firstOperand.textContent ="0";
         }
 }
+
 clearButton.addEventListener('click',()=> {
-    firstOperand.textContent = "";
+    firstOperand.textContent = "0";
     secondOperand.textContent = "";
     operator = "";
 })
+
 numericButtons.forEach(button =>{
     button.addEventListener("click",()=>{
-         appendNumber(button.innerText);
+         appendNumber(button.textContent);
 
     })
 })
+
 operatorButtons.forEach(button =>{
     button.addEventListener("click",(e)=>{
         isExpressionReady(e);
         lastOperatorIsEquals= false;
     })
 })
+
 equalsButton.addEventListener("click", button =>{
     let expressionArray = [secondOperand.textContent,operator,firstOperand.textContent];
     if (expressionArray.includes("")){return}
